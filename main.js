003df3b86cec2c841bb7c151056967c087138cdd
Saisii <sethconger@gmail.com>
2022-11-04 20:48:29 +0300

    create one div grid block

---
+++ main.js
@@ -0,0 +1,6 @@
+const gridContainer = document.querySelector("#grid");
+
+const gridContent = document.createElement('div');
+gridContent.classList.add('grid_box');
+
+gridContainer.appendChild(gridContent);
