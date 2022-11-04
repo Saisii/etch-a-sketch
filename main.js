1580ef3e369418b1c77149f08f8caffce84f3e2f
Saisii <sethconger@gmail.com>
2022-11-04 21:33:46 +0300

    selector all boxes and change color on hover

--- main.js
+++ main.js
@@ -7,7 +7,15 @@ function makeBox () {
     gridContainer.appendChild(gridContent);
 }
 
-for (i = 0; i <= 128; i++) {
+// create a grid of blocks
+for (i = 0; i <= 1000; i++) {
     makeBox();
 }
 
+const colorBox = document.querySelectorAll('.grid_box');
+
+for (let i = 0; i < colorBox.length; i++) {
+    colorBox[i].addEventListener('mouseover', () => {
+        colorBox[i].style.backgroundColor = 'red';
+})
+}
